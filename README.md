8bcf642b9dc714235b59314ffafb9834a4d8b625
neulhan <neulhan@lawandgood.com>
2020-11-28 23:19:26 +0900

    project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Canvas Animation
+
+kimbyungchan님의 velog 글 canvas animation 클론 코딩
+https://velog.io/@kimbyungchan/canvas-animation
+
+- canvas starcraft 너무 따라해보고 싶어서 kimbyungchan님의 첫 글 부터 정주행 시작
+
+![](./preview.png)
---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import Shape from "./Shape";
+import Vector from "./Vector";
+import Circle from "./Circle";
+
+class App {
+  canvas: HTMLCanvasElement;
+  context: CanvasRenderingContext2D;
+  delta: number = 0;
+  startTime: number;
+  frameRequestHandle: number;
+  stageWidth: number;
+  stageHeight: number;
+  shapes: Array<Shape> = [];
+
+  constructor() {
+    this.stageWidth = 0;
+    this.stageHeight = 0;
+    this.canvas = document.createElement("canvas");
+    this.context = this.canvas.getContext("2d")!;
+    this.startTime = Date.now();
+    this.frameRequestHandle = window.requestAnimationFrame(this.frameRequest);
+    document.body.appendChild(this.canvas);
+
+    this.resize();
+
+    window.addEventListener("resize", this.resize.bind(this));
+    for (let i = 0; i < 100; i++) {
+      this.shapes.push(
+        new Circle(new Vector(this.stageWidth * 0.5, this.stageHeight * 0.5))
+      );
+    }
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+    this.canvas.width = this.stageWidth;
+    this.canvas.height = this.stageHeight;
+  }
+
+  frameRequest = () => {
+    this.frameRequestHandle = window.requestAnimationFrame(this.frameRequest);
+
+    const currentTime = Date.now();
+    this.delta = (currentTime - this.startTime) * 0.001;
+    this.startTime = currentTime;
+
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    for (let i = 0; i < this.shapes.length; i++) {
+      this.shapes[i].update(this.delta);
+      this.shapes[i].render(this.context);
+    }
+  };
+}
+
+window.addEventListener("load", () => {
+  new App();
+});
